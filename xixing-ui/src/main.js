4c01a3c40ed9cb2c6c323654f9dd7b1e65ab3469
Pip190 <wjk152153431@outlook.com>
2023-08-02 00:18:40 +0800

    初始element-plus

--- xixing-ui/src/main.js
+++ xixing-ui/src/main.js
@@ -2,5 +2,9 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+const app = createApp(App)
+app.use(ElementPlus, { size: 'small', zIndex: 3000 })
+app.mount('#app')
+// createApp(App).mount('#app')
